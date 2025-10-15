dd7768c14129cc465185712a49a3c52f9851fe65
micheledinelli <dinellimichele00@gmail.com>
2025-10-15 11:36:31 +0200

    chore(nip): update response models

--- backend-nip/models/auth.go
+++ backend-nip/models/auth.go
@@ -4,7 +4,7 @@ import "time"
 
 type PACRequestModel struct {
 	PID       string `json:"pid" example:"DmIq8x2JNs+E2qGBr16LxP6lUK+i/5nJRLuzvYEymtY=" binding:"required"`
-	SignedPID string `json:"signed_pid" example:"fzLD1/PAJoq7EtWqIXLOYMhxNJCK9L4w4yniXZSxu0r3+oZ84jreOAhAIlrodSdg0/4EaKYWLmyf0D0uD0eozzwhvVYPDZIrLSxllH+HeEWWuOm1hnPVOOUwo7G4ROemojqwNQ6uvSB+AxoAPiSfm01Rgi0BPoaH9X8H6r94O9Hm3ZrgA9zoxQhMTx/eDpsW2yNbfPFXd18IAuHqLJSCBOSl3U4PfHEad3xFw6H04s2hr3Rqlsb2WsJmS6rf0mPEPAKjq1fSvBIcxg6PCJdfw/GhZEByrP0qc5DllkYSYZaddM/pAh+8LA16YmN1eO9VthsQxGt/WHma/5xsChOaCMd+fgk+Yjio56LoHrC86SqKsS4gkfa2jZoyagwNCah/nKAQkeVzQjk5+WLdCfMh71v82lw9L3rwlng2KhLC4z19k7wheoKQipJaxFR72T7PDftt1TvttK5q6F8x+sV6cTC6roRkgZG03X4tgLJrkBLjF9w3KbcKhPzLyu9ehYtTqFe9hBuR11UXirTi96bvmT9wWDVoTxv+ZGvn59e0VLBcQp67W2m3CZgmIs5eksQPhsFufRwYq49kXxFqnX5OZdqmVVhbQM8X9b8dTpgxd+tIuPzrYcG3ts5ApPCdXMoM/s1Tp6x+QQtq6uOD44V5v5E9tHd9GAH5cYKDfPvdA/w=" binding:"required"`
+	SignedPID string `json:"signed_pid" example:"signed_pid_payload_base64" binding:"required"`
 }
 
 type PACVerificationRequestModel struct {
--- backend-nip/models/identity.go
+++ backend-nip/models/identity.go
@@ -1,11 +1,11 @@
 package models
 
 type PIDRequestModel struct {
-	PublicKey string `json:"public_key" example:"abc123def456ghci78" binding:"required"`
+	PublicKey string `json:"public_key" example:"-----BEGIN RSA PUBLIC KEY-----MIIBCgKCAQEA2aOVUGMBXS07BIKnFq0w/enJLXtQ1K/Yq0ep+ov68R+TPO7EafnZ/fzZSFygAVjYTkgHWr6fXwp4xtPdUUVYMYJLQG5gqK6oOlONZO1NKwjb24Ww6ViRnxYUJOld/6AlZ6kKHDneVI1aINkjJqx6YMK82u5m/7G1/xeHQ9geeQL/CKCAIf+rKGTHYVYQRPIvKhoEMWmYlrAsHpzW462UZDu/zRMqoQXn+KvXI/WrIvisOwXZLoBve8gA6aLbtxOCCmZ/ARK/SeEJrp1mcnRdHVkUHvveQARkoT3dLEeRmLQf9N1HYcLC8GwcWaY0v7cCv4nCVgvJaJfQzA8SngSQqQIDAQAB-----END RSA PUBLIC KEY-----" binding:"required"`
 }
 
 type PIDResponseModel struct {
-	PID     string `json:"pid" example:"a1b2c3d4-e5f6-7890-1234-567890abcdef"`
+	PID     string `json:"pid" example:"Nifl3y+2jmuAxF26jqpjogu0ZYnA6IxSikjmTnnjm7k="`
 	Message string `json:"message" example:"PID successfully issued and saved"`
 }
 
