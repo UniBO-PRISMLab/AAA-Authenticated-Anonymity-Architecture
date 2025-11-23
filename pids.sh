1131dd3190eb5fdfed45bc47e7802e3572390302
micheledinelli <dinellimichele00@gmail.com>
2025-11-23 11:24:02 +0100

    feat(nip-backend): add script to generate public identites

--- pids.sh
+++ pids.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 for i in $(seq 1 100);
 do
     echo "Generating pid $i"
@@ -8,7 +10,7 @@ do
     PUBLIC_KEY_B64=$(base64 -i public.key | tr -d '\n')
 
     PID=$(curl -X 'POST' \
-    'http://localhost:8888/v1/identity/pid' \
+    'http://127.0.0.1:8888/v1/identity/pid' \
     -H 'accept: application/json' \
     -H 'Content-Type: application/json' \
     -d '{
